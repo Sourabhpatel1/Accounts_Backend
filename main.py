345a3026d6853fe7707521133d6376f27f116b7c
Sourabh Patel <51544244+Sourabhpatel1@users.noreply.github.com>
2023-05-16 17:37:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ origins = [
 ]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins = origins,
-    allow_methods=["*"]
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
